f0aca68b816d63e2ff46e9b371a3710033565470
M4XD4B0ZZ <56947356+M4XD4B0ZZ@users.noreply.github.com>
2026-03-21 21:58:49 +0100

    chore(eslintrc): add override rule for src/test-setup.ts

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,4 +2,12 @@ module.exports = {
   root: true,
   extends: ['expo', 'prettier'],
   ignorePatterns: ['supabase/functions/**'],
+  overrides: [
+    {
+      files: ['src/test-setup.ts'],
+      rules: {
+        '@typescript-eslint/no-require-imports': 'off',
+      },
+    },
+  ],
 };
